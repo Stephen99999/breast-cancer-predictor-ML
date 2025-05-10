9a46e59bb0b2cce047f8b301084c2b383a513310
stephen9999 <stephendivine3@gmail.com>
2025-05-10 16:43:04 +0100

    done

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import joblib
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Allow frontend access
+
+model = joblib.load('model/logistic_regression_top_features.pkl')
+scaler = joblib.load('model/scaler.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['features']
+    data = np.array(data).reshape(1, -1)
+    scaled = scaler.transform(data)
+    prediction = model.predict(scaled)[0]
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
